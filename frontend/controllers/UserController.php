b463e9f30c2dae729b43ae6978ef2c5a8403a724
gaotiefeng <1096392101@qq.com>
2019-12-19 09:35:40 +0800

    update user index

--- frontend/controllers/UserController.php
+++ frontend/controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace frontend\controllers;
 
 use frontend\job\demo;
+use frontend\services\UserServices;
 use Yii;
 use common\models\User;
 use common\models\UserSearch;
@@ -30,8 +31,10 @@ class UserController extends SiteController
         $offset = $request->get('offset',0);
         $limit = $request->get('limit', 10);
 
+        $user = new UserServices();
+        $result = $user->index($offset,$limit);
 
-        $this->success($params,'请求成功');
+        $this->success($result,'请求成功');
     }
 
     /**
@@ -46,11 +49,11 @@ class UserController extends SiteController
 
 
     /**
-     * Deletes an existing User model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
+     * @param $id
+     * @return false|int
+     * @throws NotFoundHttpException
+     * @throws \Throwable
+     * @throws \yii\db\StaleObjectException
      */
     public function actionDelete($id)
     {
